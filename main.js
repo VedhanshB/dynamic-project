1109098a491ae4fae43fe18f81b02c68db83de2a
VedhanshB <90380295+VedhanshB@users.noreply.github.com>
2023-04-26 17:42:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+status = '';
+objects = [];
+song = '';
+
+function preload() {
+    song = loadSound('alarm.mp3');
+}
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.size(380, 380)
+    video.hide()
+    objectDectector = ml5.objectDetector('cocossd', modelLoaded)
+    document.getElementById('status').innerHTML = 'Status : Detecting Objects'
+}
+
+function modelLoaded() {
+    console.log('Model Loaded!')
+    status = true
+    objectDectector.detect(video, gotResult)
+}
+
+function gotResult(error, results){
+    if (error) {
+        console.log(error)
+    }
+    console.log(results)
+    objects = results
+}
+
+function draw(){
+    image(video, 0, 0, 380, 380)
+
+    if(status != "")
+    {
+        r = random(255)
+        g = random(255)
+        b = random(255)
+
+        objectDectector.detect(video, gotResult)
+
+        for (i = 0; i < objects.length; i++){
+            document.getElementById('status').innerHTML = 'Status : Object Detected';   
+
+            fill(r,g,b)
+            percent = floor(objects[i].confidence * 100)
+            text(objects[i].label + '' + percent + '%', objects[i].x + 15, objects[i].y + 15)
+            noFill()
+            stroke(r,g,b)
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+
+            if (objects[i].label == 'person'){
+                document.getElementById('numofobj').innerHTML = 'Baby Found'
+                console.log('Song Stopped')
+                song.stop()
+            } else {
+                document.getElementById('numofobj').innerHTML = 'Baby Not Found'
+                song.play()
+                console.log('Song is Playing')
+            }
+        }
+        if (objects.length == 0 ) {
+            document.getElementById('numofobj').innerHTML = 'Baby Not Found'
+            song.play()
+            console.log('Song is Playing')
+        }
+    }
+}
